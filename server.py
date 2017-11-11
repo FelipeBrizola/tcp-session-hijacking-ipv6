801b5986b29d5b15b01bc899af8386edbea08c2c
Felipe Brizola <felipe_brizola@hotmail.com>
2017-11-11 17:50:24 -0200

    printando request e ajuste de identacao

--- server.py
+++ server.py
@@ -12,14 +12,15 @@ tcpSerSock.bind(ADDR)
 tcpSerSock.listen(5)
 
 while True:
-        print('Waiting for connection...')
-        tcpCliSock, addr = tcpSerSock.accept()
-        print('...connected from:', addr)
+    print('Waiting for connection...')
+    tcpCliSock, addr = tcpSerSock.accept()
+    print('...connected from:', addr)
 
-        while True:
-                data = tcpCliSock.recv(BUFSIZ)
-                if not data:
-                        break`
-                tcpCliSock.send(('[%s] %s'%(bytes(ctime(), 'utf-8'), data)).encode('utf-8'))
-        tcpCliSock.close()
+    while True:
+        data = tcpCliSock.recv(BUFSIZ)
+        if not data:
+            break
+        print data
+        tcpCliSock.send(data)
+    tcpCliSock.close()
 tcpSerSock.close()
